348ae2d4efdff81746a440e7fb6aff0ff1fdd929
ThayanaRMachado <thayanarm@hotmail.com>
2021-03-20 21:38:34 -0300

    setup do projeto

---
+++ src/test/java/br/com/zupacademy/thayana/casadocodigo/CasaDoCodigoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zupacademy.thayana.casadocodigo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CasaDoCodigoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
